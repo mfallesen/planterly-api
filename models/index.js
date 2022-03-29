8c9a8e77c5e33401de950197c9926a352b04b798
Mike Fallesen <mfallesen2010@gmail.com>
2022-03-29 14:53:56 -0700

    chore(boilerplate): setup some basic bolerplate for the server

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const User = require("./User");
+
+module.exports = { User, };
+
+
+// User Plant Obejcts should look like this
+
+let userPlants = {
+    [ 
+        {
+            plant_id: "id",
+            plant_name: "plant_name",
+            plant_type: "plant Type",
+        }
+    ]
+}
