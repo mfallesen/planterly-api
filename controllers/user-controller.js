5cf06cec6a35418850d56d38b8baf413ad9727a8
Mike Fallesen <mfallesen2010@gmail.com>
2022-04-26 15:53:50 -0700

    feature(authentication): rework authenitcation to store a token to check against to verify user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,35 +1,6 @@
 const { User, Thought } = require("../models");
 
 const userController = {
-  // get all users
-  // getUsers(req, res) {
-  //   User.find()
-  //     .select('-__v')
-  //     .then((dbUserData) => {
-  //       res.json(dbUserData);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //       res.status(500).json(err);
-  //     });
-  // },
-  // get single user by id
-  // getSingleUser(req, res) {
-  //   User.findOne({ _id: req.params.userId })
-  //     .select('-__v')
-  //     .populate('friends')
-  //     .populate('thoughts')
-  //     .then((dbUserData) => {
-  //       if (!dbUserData) {
-  //         return res.status(404).json({ message: 'No user with this id!' });
-  //       }
-  //       res.json(dbUserData);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //       res.status(500).json(err);
-  //     });
-  // },
   // create a new user
   createUser(req, res) {
     User.create(req.body)
@@ -45,22 +16,21 @@ const userController = {
   // log in user
   loginUser(req, res) {
     User.findOne({ username: req.body.username }).then((userInfo) => {
-      console.log("++++++");
-      console.log(userInfo);
-      console.log("++++++");
       if (!req.body.password) {
         res.status(401).send("Please Include a Password");
       }
 
       userInfo.passwordVerify(req.body.password, function (err, isMatch) {
-        // console.log(userInfo);
         if (err) throw err;
-        console.log(userInfo.password, isMatch);
-        if (isMatch) res.status(200).send("user logged in");
+        if (isMatch) {
+          res.cookie(req.body.userFirstName, req.body.username);
+          res.status(200).send("User Found and logged in!");
+        }
       });
     });
   },
   // update a user
+  // +++++NOT IMPLEMENTED
   updateUser(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
@@ -83,7 +53,8 @@ const userController = {
         res.status(500).json(err);
       });
   },
-  // delete user (BONUS: and delete associated thoughts)
+  // delete user
+  // +++++NOT IMPLEMENTED
   deleteUser(req, res) {
     User.findOneAndDelete({ _id: req.params.userId })
       .then((dbUserData) => {
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('express');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+const cookieParser = require("cookie-parser");
+const COOKIESIGN = process.env.C;
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(cookieParser(COOKIESIGN));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+console.log(process.env.C);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
